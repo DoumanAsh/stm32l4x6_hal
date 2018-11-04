b1bcd07efaa541670b7f0f15cfb9dc8c3a23f410
Travis CI <douman@gmx.se>
2018-11-04 08:45:22 +0000

    Auto-update
    Commit: Use debug_assert macros(020b47d 2018-11-04)

--- STM32L496AG/embedded_hal/timer/sidebar-items.js
+++ STM32L496AG/embedded_hal/timer/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["CountDown","A count down timer"],["Periodic","Marker trait that indicates that a timer is periodic"]]});
+initSidebarItems({"trait":[["Cancel","Trait for cancelable countdowns."],["CountDown","A count down timer"],["Periodic","Marker trait that indicates that a timer is periodic"]]});
