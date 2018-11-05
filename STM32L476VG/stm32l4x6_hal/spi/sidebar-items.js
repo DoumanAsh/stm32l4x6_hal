90f9b1576c26421c558fdc6e2a29be3468b0562d
Travis CI <douman@gmx.se>
2018-11-05 06:12:26 +0000

    Auto-update
    Commit: -m Clean-up SPI device specific module(73b668b 2018-11-05)

--- STM32L476VG/stm32l4x6_hal/spi/sidebar-items.js
+++ STM32L476VG/stm32l4x6_hal/spi/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error","SPI errors."]],"mod":[["stm32l476vg",""]],"struct":[["Spi","SPI"]],"trait":[["InnerSpi","Describes raw SPI from device crate"],["MISO","Describes MISO Pin"],["MOSI","Describes MOSI Pin"],["SCK","Describes SCK Pin"]]});
+initSidebarItems({"enum":[["Error","SPI errors."]],"struct":[["Spi","SPI"]],"trait":[["InnerSpi","Describes raw SPI from device crate"],["MISO","Describes MISO Pin"],["MOSI","Describes MOSI Pin"],["SCK","Describes SCK Pin"]]});
