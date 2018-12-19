7812ac19ba6e51221d4335f5b403574bbff116a8
Travis CI <douman@gmx.se>
2018-12-19 09:02:27 +0000

    Auto-update
    Commit: Initial draft for Serial interface(ca11141 2018-12-19)

---
+++ STM32L476VG/implementors/embedded_hal/serial/trait.Read.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["stm32l4x6_hal"] = [{text:"impl&lt;UART:&nbsp;<a class=\"trait\" href=\"stm32l4x6_hal/serial/trait.RawSerial.html\" title=\"trait stm32l4x6_hal::serial::RawSerial\">RawSerial</a>, T:&nbsp;<a class=\"trait\" href=\"stm32l4x6_hal/serial/trait.TX.html\" title=\"trait stm32l4x6_hal::serial::TX\">TX</a>, R:&nbsp;<a class=\"trait\" href=\"stm32l4x6_hal/serial/trait.RX.html\" title=\"trait stm32l4x6_hal::serial::RX\">RX</a>, C:&nbsp;<a class=\"trait\" href=\"stm32l4x6_hal/serial/trait.CK.html\" title=\"trait stm32l4x6_hal::serial::CK\">CK</a>&gt; <a class=\"trait\" href=\"embedded_hal/serial/trait.Read.html\" title=\"trait embedded_hal::serial::Read\">Read</a>&lt;u8&gt; for <a class=\"struct\" href=\"stm32l4x6_hal/serial/struct.Serial.html\" title=\"struct stm32l4x6_hal::serial::Serial\">Serial</a>&lt;UART, T, R, C&gt;",synthetic:false,types:["stm32l4x6_hal::serial::Serial"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
